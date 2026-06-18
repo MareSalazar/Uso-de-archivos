392f563feeb14acdc867d7f6b0f1198242470a7c
Mare Salazar <mare.salazar790@gmail.com>
2026-06-18 15:31:13 -0500

    Ultimo commit, uso de archivos, ojo con las vriables y punteros

--- Archivos.c
+++ Archivos.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdio.h>
 
 typedef struct  {
     char callePrincipal[50];
@@ -13,28 +14,29 @@ typedef struct {
 } Alumno;
 
 void mostrarAlumno(Alumno *a);
-void guardarArchivo();
+void guardarArchivo(Alumno *a);
 
 
 int main(){
     //p1 ahora se va a llamar clase, estoy creando un arreglo
     Alumno clase[2];
     strcpy(clase[0].nombre, "Juan");
-    strcpy(clase[0].direccion.callePrincipal, "Av. 10 de Diciembre" );
-    strcpy(clase[0].direccion.calleSecundaria, "Republica");
+    strcpy(clase[0].direccion.callePrincipal, "Av. Prensa" );
+    strcpy(clase[0].direccion.calleSecundaria, "Florida");
     clase[0].direccion.numCasa=123;
     clase[0].edad= 20;
     strcpy(clase[1].nombre, "Maria");
     strcpy(clase[1].direccion.callePrincipal, "Av.Galo Plaza" );
-    strcpy(clase[1].direccion.calleSecundaria, "Albeniz");
+    strcpy(clase[1].direccion.calleSecundaria, "Inca");
     clase[1].direccion.numCasa=123;
     clase[1].edad= 25;
     
     mostrarAlumno(clase);
-    
+    guardarArchivo(clase);
     
     return 0; 
 }
+
 void mostrarAlumno(Alumno *a){
     for (int i=0; i<2; i++){
     printf ("Nombre:%s \n", a[i].nombre);
@@ -46,8 +48,18 @@ void mostrarAlumno(Alumno *a){
     }
 };
 
-void guardarArchivo(){
-    FILE *archivo = ("alumnos.txt","w");
+void guardarArchivo(Alumno *a){
+    FILE *archivo = fopen ("alumnos.txt","w");
     //(.txt) es para referirnos a archivos planos, es decir solo de texto
     //Modificador de acceso write (w)
+    for (int i; i<2; i++){
+    fprintf(archivo, "Nombre:%s \n" , a[i].nombre);
+    //fprintf necesita el paso de referencia, primero el nombre de el puntero y el dato
+    fprintf(archivo, "Edad: %d \n " , a[i].edad);
+    fprintf(archivo,"Direccion: %s %d %s", a[i].direccion.callePrincipal,a[i].direccion.numCasa, a[i].direccion.calleSecundaria );
+
+    }
+    fclose(archivo);
+    
 };
+ 
